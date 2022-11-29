f2fefa61d324dcadb09801ded402ef0ab372c60b
oliver <oliver.pecek@gmail.com>
2022-11-29 15:45:17 +0100

    removed extended generate, bc useless password main fin

--- src/password/Password.java
+++ src/password/Password.java
@@ -5,26 +5,18 @@ import java.util.Random;
 public class Password {
     public Password() {
     }
-    
+
     public String generate() {
         return this.generate(20);
     }
 
     public String generate(int length) {
-        return generate(length, false);
-    }
-
-    public String generate(int length, boolean extended) {
         StringBuilder generatedPassword = new StringBuilder();
         Random random = new Random();
-        int bound = 0;
-        if (extended) {
-            bound = 256;
-        } else {
-            bound = 128;
-        }
+        int bound = 96;
         while (generatedPassword.length() < length) {
-            generatedPassword.append((char) random.nextInt(bound));
+            int ascii = random.nextInt(bound) + 32;
+            generatedPassword.append((char) ascii);
         }
         return generatedPassword.toString();
     }
--- src/password/PasswordMain.java
+++ src/password/PasswordMain.java
@@ -2,6 +2,8 @@ package password;
 
 public class PasswordMain {
     public static void main(String[] args) {
-
+        Password password = new Password();
+        System.out.println(password.generate());
+        System.out.println(password.generate(35));
     }
 }
