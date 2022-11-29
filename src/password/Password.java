da4a6f7c3f740c553d8aa0f873013702f0a863ad
oliver <oliver.pecek@gmail.com>
2022-11-29 14:41:45 +0100

    created Password.java

---
+++ src/password/Password.java
@@ -0,0 +1,17 @@
+package password;
+
+public class Password {
+    private String password;
+
+    public Password() {
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String generatePassword(int length) {
+        StringBuilder generatedPassword = new StringBuilder();
+        return generatedPassword.toString();
+    }
+}
