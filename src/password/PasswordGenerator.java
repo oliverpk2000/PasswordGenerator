26376f277a04406a41a9645e09a0706fef4ddcb1
oliver <oliver.pecek@gmail.com>
2022-11-30 09:23:22 +0100

    user input done

--- src/password/PasswordGenerator.java
+++ src/password/PasswordGenerator.java
@@ -1,7 +1,12 @@
 package password;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        
+        Password password = new Password();
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.print("length: ");
+        System.out.println(password.generate(inputScanner.nextInt()));
     }
 }
